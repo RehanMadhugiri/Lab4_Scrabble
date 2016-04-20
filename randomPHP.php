311b74b627ddd449122e35902436646f4bdb6b4a
RehanMadhugiri <rehan.madhugiri@gmail.com>
2016-04-20 15:13:05 -0400

    Added files via upload

--- randomPHP.php
+++ randomPHP.php
@@ -1,33 +1,57 @@
 <?php
-	/*$numLetters = $_REQUEST["numL"]; 
-	$matches = array();*/
+		
+		$main = array();
+		$temp = array();
+		$string  = strtolower($_REQUEST["str"]);
+		$first = substr($string, 0, 1);
+		echo "$string";
+		echo "$first";
 
-	$filename = "wordsEn.txt";
-/*
-	$pattern = "{".$numLetters-1."}";
 
-	preg_match_all($pattern, "output.txt", $matches);
-	var_dump($matches);
-*/
-	/*$file = fopen($filename, "r");
-	$i = 0;
-	while (!feof($file)) {
-		$line_of_text = fgets($file);
-		$members = explode('\n', $line_of_text);
-		fclose($file);
-	}*/
+		function update(){
+    		global $main, $temp;
+    		$count = 0;
+    		$main = array();
+   
+    		foreach ($temp as &$value) {
+        		$main[] = trim($value);
+    		}
+   		 unset($value); // break the reference with the last element
 
-	$file = fopen($filename, "r");
-		$members = array();
+    //$main = $temp;
+    $temp = array();
+}
 
-		while (!feof($file)) {
-   			$members[] = fgets($file);
+
+		$myfile = fopen("wordsEn.txt", "r") or die("Unable to open file!");
+		while(!feof($myfile)) { 
+            
+    		$current = fgets($myfile); // gets the current word in txt file
+    		$main[] = $current; // adds to main array    
 		}
+		fclose($myfile);
 
-		fclose($file);
 
-		var_dump($members);
+		//1. first letter(s)
+		if($first != null){
+    
+    		for($x = 0; $x < sizeof($main); $x++) {
 
+        		$current = $main[$x];
+       		 	$number = substr_compare($first,$current,0,strlen($first),TRUE); // compares first letters of input and first letters of current word
+       		 	echo "$number";
+
+        	if($number == 0){
+           		 $temp[] = $current; 
+           		 echo "success";
+        	}          
+        	update();
+    }
+
+
+}
+
+echo implode(",", $main);
 
 
 ///http://stackoverflow.com/questions/6159683/read-each-line-of-txt-file-to-new-array-element
